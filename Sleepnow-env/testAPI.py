f13613b62e3eec664bf73174ffa76c18ddae0522
Dan3tte <danielbenoit25@gmail.com>
2025-07-17 13:28:47 +0200

    localclient

--- Sleepnow-env/testAPI.py
+++ Sleepnow-env/testAPI.py
@@ -1,17 +1,24 @@
 import os
-import valorant
+from valorant import LocalClient
 from dotenv import load_dotenv
 load_dotenv()
 
 
-KEY = os.environ["API_KEY"]
+client = LocalClient()
 
-client = valorant.Client(KEY, locale=None)
-skins = client.get_skins()
-name = input("Search a Valorant Skin Collection: ")
+status = client.get_session()
 
-results = skins.find_all(name=lambda x: name.lower() in x.lower())
 
-print("\nResults: ")
-for skin in results:
-    print(f"\t{skin.name.ljust(21)} ({skin.localizedNames['ja-JP']})")
+# Exemple d’accès possible à l'état (à adapter selon le nom de la propriété)
+print("Phase:", session.phase)
+# ou
+print("InGame:", session.in_game)
+#skins = client.get_skins()
+
+#name = input("Search a Valorant Skin Collection: ")
+
+#results = skins.find_all(name=lambda x: name.lower() in x.lower())
+
+#print("\nResults: ")
+#for skin in results:
+ #   print(f"\t{skin.name.ljust(21)} ({skin.localizedNames['ja-JP']})")
